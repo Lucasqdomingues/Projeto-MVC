397d1104d13c82ce6791cb579907dd096a60077f
Lucas <lucasquintanadomingues@gmail.com>
2023-09-11 19:59:58 -0300

    first commit

---
+++ view/professor/ProfessorView.php
@@ -0,0 +1,128 @@
+<?php $professores = $_REQUEST["professores"]; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Professores</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+</head>
+
+<body>
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Atenção</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Você deseja realmente excluir esse registro ?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="close-modal">Sair</button>
+                    <button type="button" class="btn btn-danger" id="delete-button">EXCLUIR</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="userDeleted" tabindex="-1" aria-labelledby="userDeletedLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="userDeletedLabel">Parabéns</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Usuário deletado com sucesso!
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/' . FOLDER . '/view/navbar.php'; ?>
+
+    <div class="container">
+
+        <div class="row text-center">
+            <h2>Semana da Acessibilidade</h2>
+        </div>
+
+        <div class="text-center">
+            <img class="rounded" src="https://cta.ifrs.edu.br/wp-content/uploads/sites/3/2018/12/18_BoasPrativasDescricaoImagens.jpg" alt="Representação grafica de como utilizar a tag alt em uma imagem para pessoas com deficiencia visual.">
+        </div>
+
+        <br>
+        <a href="/<?php echo FOLDER; ?>/?controller=Professor&acao=salvar" class="btn btn-success">Cadastrar Professor</a>
+        <br>
+        <br>
+
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Idade</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($professores as $professorAtual) { ?>
+                    <tr>
+                        <td><?php echo $professorAtual["id"]; ?></td>
+                        <td><?php echo $professorAtual["nome"]; ?></td>
+                        <td><?php echo $professorAtual["idade"]; ?></td>
+                        <td>
+                        <a href="/<?php echo FOLDER; ?>?controller=Professor&acao=editar&id=<?php echo $professorAtual['id']; ?>" class="btn btn-primary">Editar</a>
+                        <!--<a href="/<?php echo FOLDER; ?>?controller=Estudante&acao=excluir&id=<?php echo $professorAtual['id']; ?>" class="btn btn-primary">Excluir</a> -->
+                        <button type="button" class="btn btn-primary select-user-to-delete" data-bs-toggle="modal" data-bs-target="#staticBackdrop" data-id="<?php echo $professorAtual['id']; ?>">
+                            Excluir
+                        </button>
+                    </td>
+                    </tr>
+
+
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+
+    <script>
+        $("#delete-button").on("click", function() {
+            let idUsuario = $(this).attr('data-id');
+            let url = "/<?php echo FOLDER; ?>/?controller=Professor&acao=excluir&id=" + idUsuario;
+            $.get(url, function(data) {
+                $("#close-modal").click();
+
+                var myModal = new bootstrap.Modal(document.getElementById('userDeleted'))
+                myModal.show();
+
+            });
+
+            console.log("O usuário para ser deletado é: " + idUsuario);
+        });
+
+        $("#userDeleted").on("hidden.bs.modal", function() {
+            location.reload();
+        })
+
+        $(".select-user-to-delete").on("click", function() {
+
+            $("#delete-button").attr("data-id", $(this).attr('data-id'));
+            console.log("O usuário escolheu o estudante que talvez possa ser deletado");
+        });
+    </script>
+
+
+</body>
+
+</html>
